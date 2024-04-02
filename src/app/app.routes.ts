b227b007475ab399edd1bbb63b94657d802f7057
Thales Tadashi Umekawa <thales.ume@gmail.com>
2024-04-01 23:43:56 -0300

    add navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './views/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '' , component: NavbarComponent
+    }
+];
